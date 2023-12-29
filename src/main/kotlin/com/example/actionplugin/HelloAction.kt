d862855bc05091c5935b4d91a86d102ae0b6842c
Mikhail Rodionychev <tralllgm@gmail.com>
2023-12-29 13:28:27 +0100

    plugin done

---
+++ src/main/kotlin/com/example/actionplugin/HelloAction.kt
@@ -0,0 +1,11 @@
+package com.example.actionplugin
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.ui.Messages
+
+class HelloAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        Messages.showInfoMessage("Hello, world!", "Hello Action")
+    }
+}
